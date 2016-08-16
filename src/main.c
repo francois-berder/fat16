1fee69efeb4ee54a1d4ebf32ee5f51650f90d79d
Francois Berder <fberder@outlook.fr>
2016-08-16 20:19:38 +0100

    define main and cmakelists
    Signed-off-by: Francois Berder <fberder@outlook.fr>

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFAULT_FS_PATH     "data/fs.img"
+
+
+int main(int argc, char **argv)
+{
+    char fs_path[255];
+
+    if (argc < 2) {
+        printf("Using default filesystem path\n");
+        strcpy(fs_path, DEFAULT_FS_PATH);
+    } else {
+        strcpy(fs_path, argv[1]);
+    }
+
+
+    return 0;
+}
