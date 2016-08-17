377f78eec106ce00782ce6fa26744a2ab10bc54e
Francois Berder <fberder@outlook.fr>
2016-08-17 09:06:25 +0100

    linux_hal: load/release image file
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- src/linux_hal.c
+++ src/linux_hal.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include "linux_hal.h"
 
+static FILE *image = NULL;
 
 int linux_load_image(char *path)
 {
-    printf("linux_load_image: not implemented\n");
-    return -1;
+    if (path == NULL) {
+        printf("linux_load_image: Cannot load image with null path\n");
+        return -1;
+    }
+
+    if ((image = fopen(path, "rb")) == NULL) {
+        printf("linux_load_image: Could not load file %s\n", path);
+        return -1;
+    }
+
+    return 0;
 }
 
 int linux_release_image(void)
 {
-    printf("linux_release_image: not implemented\n");
-    return -1;
+    if (image == NULL)
+        return 0;
+
+    if (fclose(image) == EOF)
+        return -1;
+
+    return 0;
 }
 
 int linux_read(uint8_t *buffer, uint32_t length)
