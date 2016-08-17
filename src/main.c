ec0b9d76c763204440a3c905d8103ec163d3bfc5
Francois Berder <fberder@outlook.fr>
2016-08-17 09:13:32 +0100

    main: load/release image
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "linux_hal.h"
 
 #define DEFAULT_FS_PATH     "data/fs.img"
 
@@ -16,6 +17,20 @@ int main(int argc, char **argv)
         strcpy(fs_path, argv[1]);
     }
 
+    // Load filesystem image
+    if (linux_load_image(fs_path) < 0)
+        return -1;
+    printf("%s filesystem loaded.\n", fs_path);
+
+
+    // FAT16 operations
+
+
+    // Release filesystem image
+    if (linux_release_image() < 0)
+        return -1;
+
+    printf("%s filesystem released.\n", fs_path);
 
     return 0;
 }
