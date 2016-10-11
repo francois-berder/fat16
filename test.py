8fd98e191a642d7e1b5922f626de597f419ce6c0
Francois Berder <fberder@outlook.fr>
2016-10-11 22:09:46 +0100

    add init test

--- test.py
+++ test.py
@@ -7,14 +7,28 @@ dll_name = "libfat16_driver.so"
 dllabspath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + dll_name
 _LIB = ctypes.CDLL(dllabspath)
 
+test_records = {}
+
+def record_test_result(test_name, result):
+    test_records[test_name] = result
+
+def test_init():
+    print('----- init -----')
+    ret = _LIB.fat16_init()
+    record_test_result('init', ret == 0)
 
 def main(argv):
     _LIB.linux_load_image(str.encode(argv[1]))
 
     # Perform tests
+    test_init()
 
     _LIB.linux_release_image()
 
 if __name__ == "__main__":
     main(sys.argv)
 
+    # Print test results
+    print('\n\n\n##### TEST RESULTS #####')
+    for test_name, test_result in test_records.items():
+        print('{}: {}'.format(test_name, 'PASS' if test_result else 'FAIL'))
