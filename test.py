38490a1b2671612a3bbc7a8a5541a5b9d76fc56f
Francois Berder <fberder@outlook.fr>
2016-10-13 20:21:41 +0100

    record test results overwrite if false
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- test.py
+++ test.py
@@ -13,7 +13,14 @@ _LIB = ctypes.CDLL(dllabspath)
 test_records = {}
 
 def record_test_result(test_name, result):
-    test_records[test_name] = result
+    if not result:
+        test_records[test_name] = result
+    else:
+        if test_name in test_records.keys():
+            if test_records[test_name]:
+                 test_records[test_name] = result
+        else:
+            test_records[test_name] = result
 
 def restore_image(image_path):
     subprocess.run(['git', 'checkout' , image_path])
