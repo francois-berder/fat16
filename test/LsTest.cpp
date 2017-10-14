631885857a2122fa62685850cc10a1ce52693550
Francois Berder <fberder@outlook.fr>
2017-10-14 11:20:26 +0100

    test: Improve LsTest to better check file existence and filename
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- test/LsTest.cpp
+++ test/LsTest.cpp
@@ -1,7 +1,9 @@
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 #include <sstream>
+#include <vector>
 #include "Common.hpp"
 #include "LsTest.hpp"
 #include "../driver/fat16.h"
@@ -34,6 +36,11 @@ bool LsTest::run()
     if (fat16_init(linux_dev) < 0)
         return false;
 
+    std::vector<bool> file_exist;
+    file_exist.resize(m_files_count);
+    for (unsigned int j = 0; j < file_exist.size(); ++j)
+        file_exist[j] = false;
+
     unsigned int files_count = 0;
     char filename[13];
     uint16_t i = 0;
@@ -41,9 +48,30 @@ bool LsTest::run()
     while((ret = fat16_ls(&i, filename)) == 0) {
         ++files_count;
         printf("Found file: %s\n", filename);
+
+        /* extract file name */
+        std::string s(filename);
+        size_t lastindex = s.find_last_of(".");
+        std::string rawname = s.substr(0, lastindex);
+        long index = std::stol(rawname);
+        if (index < 0 || index >= m_files_count)
+            return false;
+        file_exist[index] = true;
+
+        /* Check filename */
+        std::string correct_filename = std::to_string(index) + ".TXT";
+        if (correct_filename != filename)
+            return false;
     }
     if (ret != -2)
         return false;
+
+    for (unsigned int j = 0; j < file_exist.size(); ++j) {
+        if (!file_exist[j]) {
+            std::cout << "Could not find file \'" << j << ".TXT\'" << std::endl;
+            return false;
+        }
+    }
 
     return files_count == m_files_count;
 }
