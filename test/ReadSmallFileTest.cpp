0dd92604765bfaced36a6b145aa934c679f5164d
Francois Berder <fberder@outlook.fr>
2017-10-21 00:19:57 +0100

    test: Add case for reading file in subdir in ReadSmallFileTest
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- test/ReadSmallFileTest.cpp
+++ test/ReadSmallFileTest.cpp
@@ -13,7 +13,11 @@ m_content("Hello, World!")
 void ReadSmallFileTest::init()
 {
     restore_image();
+    mount_image();
     create_small_file("HELLO.TXT", m_content);
+    system("mkdir /mnt/TMP");
+    create_small_file("/TMP/HELLO.TXT", m_content);
+    unmount_image();
     load_image();
 }
 
@@ -22,32 +26,50 @@ bool ReadSmallFileTest::run()
     if (fat16_init(linux_dev) < 0)
         return false;
 
-    int fd = fat16_open("HELLO.TXT", 'r');
-    if (fd < 0)
-        return false;
+    {
+        int fd = fat16_open("HELLO.TXT", 'r');
+        if (fd < 0)
+            return false;
 
-    char buf[20];
-    int ret = fat16_read(fd, buf, sizeof(buf));
-    if (ret < 0)
-        return false;
-    if (m_content != std::string(buf))
-        return false;
+        char buf[20];
+        int ret = fat16_read(fd, buf, sizeof(buf));
+        if (ret < 0)
+            return false;
 
-    if (fat16_close(fd) < 0)
-        return false;
+        buf[ret] = '\0';
+        if (m_content != std::string(buf))
+            return false;
+
+        if (fat16_close(fd) < 0)
+            return false;
+    }
+
+    {
+        int fd = fat16_open("/TMP/HELLO.TXT", 'r');
+        if (fd < 0)
+            return false;
+
+        char buf[20];
+        int ret = fat16_read(fd, buf, sizeof(buf));
+        if (ret < 0)
+            return false;
+
+        buf[ret] = '\0';
+        if (m_content != std::string(buf))
+            return false;
+
+        if (fat16_close(fd) < 0)
+            return false;
+    }
 
     return true;
 }
 
 void ReadSmallFileTest::create_small_file(const std::string &filename, const std::string &content)
 {
-    mount_image();
-
     std::string path = "/mnt/";
     path += filename;
     std::ofstream file(path);
     file << content;
     file.close();
-
-    unmount_image();
 }
