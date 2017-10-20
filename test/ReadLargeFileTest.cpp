6bd44ae48e57861b5f327170b15e88e0bb518976
Francois Berder <fberder@outlook.fr>
2017-10-21 00:18:11 +0100

    test: Add case for reading file in subdir in ReadLargeFileTest
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- test/ReadLargeFileTest.cpp
+++ test/ReadLargeFileTest.cpp
@@ -15,7 +15,11 @@ m_bytes_count(bytes_count)
 void ReadLargeFileTest::init()
 {
     restore_image();
+    mount_image();
     create_large_file("HELLO.TXT");
+    system("mkdir /mnt/TMP");
+    create_large_file("/TMP/HELLO.TXT");
+    unmount_image();
     load_image();
 }
 
@@ -24,41 +28,71 @@ bool ReadLargeFileTest::run()
     if (fat16_init(linux_dev) < 0)
         return false;
 
-    int fd = fat16_open("HELLO.TXT", 'r');
-    if (fd < 0)
-        return false;
-
-    unsigned int bytes_read_count = 0;
-    srand(2);
-    while (bytes_read_count < m_bytes_count) {
-        char expected = rand();
-        char buf = 0;
-        if (fat16_read(fd, &buf, sizeof(buf)) != 1)
-           return false;
-
-        if (buf != expected) {
-            printf("Found %02x but expected %02X\n", buf, expected);
+    {
+        int fd = fat16_open("HELLO.TXT", 'r');
+        if (fd < 0)
             return false;
+
+        unsigned int bytes_read_count = 0;
+        srand(2);
+        while (bytes_read_count < m_bytes_count) {
+            char expected = rand();
+            char buf = 0;
+            if (fat16_read(fd, &buf, sizeof(buf)) != 1)
+               return false;
+
+            if (buf != expected) {
+                printf("Found %02x but expected %02X\n", buf, expected);
+                return false;
+            }
+
+            ++bytes_read_count;
         }
 
-        ++bytes_read_count;
+        /* Check if we reached end of file */
+        char buf = 0;
+        if (fat16_read(fd, &buf, sizeof(buf)) != 0)
+            return false;
+
+        if (fat16_close(fd) < 0)
+            return false;
     }
 
-    /* Check if we reached end of file */
-    char buf = 0;
-    if (fat16_read(fd, &buf, sizeof(buf)) != 0)
-        return false;
+    {
+        int fd = fat16_open("/TMP/HELLO.TXT", 'r');
+        if (fd < 0)
+            return false;
 
-    if (fat16_close(fd) < 0)
-        return false;
+        unsigned int bytes_read_count = 0;
+        srand(2);
+        while (bytes_read_count < m_bytes_count) {
+            char expected = rand();
+            char buf = 0;
+            if (fat16_read(fd, &buf, sizeof(buf)) != 1)
+               return false;
+
+            if (buf != expected) {
+                printf("Found %02x but expected %02X\n", buf, expected);
+                return false;
+            }
+
+            ++bytes_read_count;
+        }
+
+        /* Check if we reached end of file */
+        char buf = 0;
+        if (fat16_read(fd, &buf, sizeof(buf)) != 0)
+            return false;
+
+        if (fat16_close(fd) < 0)
+            return false;
+    }
 
     return true;
 }
 
 void ReadLargeFileTest::create_large_file(const std::string &filename)
 {
-    mount_image();
-
     std::string path = "/mnt/";
     path += filename;
     std::ofstream file(path);
@@ -66,5 +100,4 @@ void ReadLargeFileTest::create_large_file(const std::string &filename)
     for (unsigned int i = 0; i < m_bytes_count; ++i)
         file.put(rand());
     file.close();
-    unmount_image();
 }
